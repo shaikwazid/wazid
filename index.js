34f9cba9b183172ff1a5aa367fe6eee460ef6c2a
shaik wazid <shaikwazid2705@gmailcom>
2023-10-25 13:20:49 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+let wishingname = ()=>{
+
+    let a = prompt("Enter Your Name");
+  
+    let name = "w-z-d"
+    if(name !== a){
+      document.getElementById("demo").innerHTML = a;
+      
+    }
+
+    
+  
+  }
